8eeb2c834ebfde5a31a3359c3e88c124f3d3c799
Ardalan <ardalan.eraghi@gmail.com>
2022-04-24 11:24:30 +0430

    create layers

---
+++ src/FizzBuzz.LogicTest/FizzBuzzTests.cs
@@ -0,0 +1,18 @@
+﻿using FizzBuzzGame;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz.LogicTest
+{
+    public class FizzBuzzTests
+    {
+        private readonly FizzBuzzLogic _sut;
+        public FizzBuzzTests()
+        {
+            _sut = new FizzBuzzLogic(); 
+        }
+    }
+}
